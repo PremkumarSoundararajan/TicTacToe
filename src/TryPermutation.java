21775677d2a6acf1547369608adfeb67e8a14b94
PremkumarSoundararajan <premkumar.soundararajan@gmail.com>
2022-11-03 07:29:35 -0600

    Handled -ve conditions

--- src/TryPermutation.java
+++ src/TryPermutation.java
@@ -3,6 +3,9 @@ import java.util.Set;
 
 public class TryPermutation {
     Set<String> possiblePermutations(String input){
+        if (input == null || input.length() == 0 || input.isBlank() || input.isEmpty())
+            return new HashSet<>();
+        
         Set<String> response = new HashSet<>();
         for (int i = 0; i < input.length();i++){
             Character C = input.charAt(i);
